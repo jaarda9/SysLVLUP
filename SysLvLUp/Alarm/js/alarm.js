b96c0ed372a63915cc1d53c8c0ee3a31ccf2b2ff
jaarda9 <132813653+jaarda9@users.noreply.github.com>
2024-11-23 00:14:50 +0100

    Update alarm.js

--- SysLvLUp/Alarm/js/alarm.js
+++ SysLvLUp/Alarm/js/alarm.js
@@ -1,4 +1,42 @@
- const savedData =JSON.parse(localStorage.getItem("gameData"));
+function resetData() {
+  const defaultGameData = {
+    level: 1,
+    hp: 100,
+    mp: 100,
+    stm: 100,
+    exp: 0,
+    fatigue: 0,
+    name: "Your Name",
+    ping: "60",
+    guild: "Reaper",
+    race: "Hunter",
+    title: "None",
+    region: "TN",
+    location: "Hospital",
+    physicalQuests: "[0/4]",
+    mentalQuests: "[0/3]",
+    spiritualQuests: "[0/2]",
+    Attributes: {
+      STR: 10,
+      VIT: 10,
+      AGI: 10,
+      INT: 10,
+      PER: 10,
+      WIS: 10,
+    },
+    stackedAttributes: {
+      STR: 0,
+      VIT: 0,
+      AGI: 0,
+      INT: 0,
+      PER: 0,
+      WIS: 0,
+    },
+  };
+  localStorage.setItem("gameData", JSON.stringify(defaultGameData));
+  location.reload();
+}
+
 const playerBtn = document.getElementById("playerBtn");
 playerBtn.addEventListener("click", function () {
   const savedData =JSON.parse(localStorage.getItem("gameData"));
@@ -12,6 +50,7 @@ playerBtn.addEventListener("click", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+ resetData();
    const savedData =JSON.parse(localStorage.getItem("gameData"));
   // Check if fullscreen mode is supported
   if (document.documentElement.requestFullscreen) {
