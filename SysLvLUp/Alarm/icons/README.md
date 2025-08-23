76a9d41a54a5dc143489a94671f152a7a4f17efa
Jaarda9 <sk1lldr1llxno@gmail.com>
2025-08-23 21:52:10 +0100

    gg

---
+++ SysLvLUp/Alarm/icons/README.md
@@ -0,0 +1,13 @@
+# PWA Icons
+
+This directory should contain the following icon files for the PWA:
+
+- `icon-192x192.png` (192x192 pixels)
+- `icon-512x512.png` (512x512 pixels)
+
+These icons are referenced in the manifest.json file and are required for proper PWA installation on devices.
+
+To create these icons:
+1. Design a simple icon that represents your app
+2. Export it in PNG format at both 192x192 and 512x512 resolutions
+3. Place the files in this directory with the exact names mentioned above
---
+++ SysLvLUp/Alarm/sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'syslvlup-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/alarm.html',
+  '/daily_quest.html',
+  '/status.html',
+  '/Initiation.html',
+  '/Quest_Info_Mental.html',
+  '/Quest_Info_Physical.html',
+  '/Quest_Info_Spiritual.html',
+  '/css/login.css',
+  '/css/alarm.css',
+  '/css/daily_quest.css',
+  '/css/status.css',
+  '/css/Initiation.css',
+  '/css/Quest_Info_Mental.css',
+  '/css/Quest_Info_Physical.css',
+  '/css/Quest_Info_Spiritual.css',
+  '/js/login.js',
+  '/js/alarm.js',
+  '/js/daily_quest.js',
+  '/js/status.js',
+  '/js/Initiation.js',
+  '/js/Quest_Info_Mental.js',
+  '/js/Quest_Info_Physical.js',
+  '/js/Quest_Info_Spiritual.js',
+  '/js/database.js',
+  '/js/sync.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
